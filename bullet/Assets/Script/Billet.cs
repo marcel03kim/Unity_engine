9d1a79e1af70985174973fb4b81c6624db3ab76a
marcel03kim <marcel03kim@gmail.com>
2024-04-15 10:42:55 +0900

    [Feat] 공유

---
+++ bullet/Assets/Script/Billet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
